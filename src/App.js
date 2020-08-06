e24a643db3f878d3e19f0a610a9cafbdb8efb968
Ronal2 <ronal2.gutierrez@gmail.com>
2020-08-05 20:54:45 -0500

    Obteniendo token de google y enviandolo al backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GoogleLogin } from "react-google-login";
 
 function App() {
+  const responseGoogle = (response) => {
+    console.log(response);
+    console.log(response.profileObj);
+
+    const id_token = response.tokenObj.id_token
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8282/loginGoogle");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onload = function () {
+      console.log("Signed in as: " + xhr.responseText);
+    };
+    xhr.send("idtoken=" + id_token);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GoogleLogin
+        clientId="213496655861-15gn0u18atkl8l8fak7s2puik0kbnb9f.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
     </div>
   );
 }
