597258de0b8d3d0e2be242bee1130fe6dafef718
Ronal2 <ronal2.gutierrez@gmail.com>
2020-08-07 17:47:51 -0500

    Diseño y saludo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoogleLogin } from "react-google-login";
+import Saludo from "./components/Saludo"
+
+import loginImg from "./login.png";
 
 function App() {
-  const responseGoogle = (response) => {
-    console.log(response);
-    console.log(response.profileObj);
+  const [userGoogle, setUserGoogle] = useState({});
 
-    const id_token = response.tokenObj.id_token
+  const responseGoogle = (response) => {
+    const id_token = response.tokenObj.id_token;
 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8282/loginGoogle");
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onload = function () {
-      console.log("Signed in as: " + xhr.responseText);
+     setUserGoogle(JSON.parse((xhr.responseText)));
     };
     xhr.send("idtoken=" + id_token);
   };
 
   return (
-    <div>
-      <GoogleLogin
-        clientId="213496655861-15gn0u18atkl8l8fak7s2puik0kbnb9f.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={"single_host_origin"}
-      />
+    <div className="section-google">
+      <div className="container-google">
+        <h1>Registro Con Google</h1>
+        <img src={loginImg} alt="imgLogin" />
+        <GoogleLogin
+          className="google"
+          clientId="213496655861-15gn0u18atkl8l8fak7s2puik0kbnb9f.apps.googleusercontent.com"
+          buttonText="Inicia sesion con Google"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+        <Saludo userGoogle = {userGoogle} />
+      </div>
     </div>
   );
 }
