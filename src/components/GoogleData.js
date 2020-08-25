6e7a77d4d9be8d47ccd24269f9b099e09382185d
Ronal2 <ronal2.gutierrez@gmail.com>
2020-08-24 19:15:43 -0500

    Peticion Get para la obtencion de los datos del usuario

--- src/components/GoogleData.js
+++ src/components/GoogleData.js
@@ -1,24 +1,40 @@
-import React from "react";
-import jwt_decode from "jwt-decode";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const GoogleData = () => {
+  const [dataUser, setDataUser] = useState({});
+
   const Storage = window.localStorage;
 
-  const token = localStorage.getItem("token");
-  const dataUser = jwt_decode(token);
-
-  const { name, email, picture } = dataUser;
+  useEffect(() => {
+    const getData = async () => {
+      const token = localStorage.getItem("token");
+      try {
+        const informationUser = await axios({
+          method: "get",
+          url: "http://localhost:8282/home",
+          headers: { token: token },
+        });
+        setDataUser(informationUser.data.user);
+      } catch (err) {
+        window.location.href = "/";
+      }
+    };
+    getData();
+  }, []);
 
   const closeSection = () => {
     Storage.clear();
     window.location.href = "/";
   };
 
+  const { name, email, img } = dataUser;
+
   return (
     <div className="section-google">
       <div className="container-google">
         <h1>Bienvenido </h1>
-        <img className="imgUser" src={picture} alt="imgLogin" />
+        <img className="imgUser" src={img} alt="imgLogin" />
         <h2>{name}</h2>
         <h2>{email}</h2>
         <button onClick={closeSection} className="cerrarSesion">
